e5d78fbd0636b0673e626c69a9d06c55a6b7a8bb
Boniface Masila <dietboni@gmail.com>
2025-09-27 16:00:03 +0300

    make changes

--- hopecare_project/settings.py
+++ hopecare_project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '']
 
 # Custom user model
 AUTH_USER_MODEL = 'accounts.User'
