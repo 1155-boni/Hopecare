39b84a22af0d1ac2293d21d78067e4ccae328975
Boniface Masila <dietboni@gmail.com>
2025-09-27 08:05:27 +0300

    Add login and logout views with templates

--- accounts/views.py
+++ accounts/views.py
@@ -12,3 +12,6 @@ def signup(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html', {'user': request.user})
