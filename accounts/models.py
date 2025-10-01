ff38df078764a6e42efd9d67bce4d709d36ef49b
Boniface Masila <dietboni@gmail.com>
2025-10-01 09:46:11 +0300

    Fix email field uniqueness for USERNAME_FIELD requirement

--- accounts/models.py
+++ accounts/models.py
@@ -16,6 +16,8 @@ class User(AbstractUser):
     date_of_admission = models.DateField(blank=True, null=True)
     admission_number = models.CharField(max_length=20, blank=True, null=True)
 
+    email = models.EmailField(unique=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
