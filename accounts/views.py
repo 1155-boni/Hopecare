2019d13eac9be789520b57403d56d852e90075e3
Boniface Masila <dietboni@gmail.com>
2025-10-01 12:38:02 +0300

    Remove BroughtByForm import from views.py as brought by step is removed

--- accounts/views.py
+++ accounts/views.py
@@ -23,7 +23,7 @@ from inventory.models import Stock
 
 logger = logging.getLogger(__name__)
 
-from .forms import CustomUserCreationForm, UserProfileForm, UserDetailsForm, BroughtByForm
+from .forms import CustomUserCreationForm, UserProfileForm, UserDetailsForm
 
 def signup(request):
     step = request.GET.get('step', '1')
